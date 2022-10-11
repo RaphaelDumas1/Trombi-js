0d4401c03581bbc9584bfa94c128e7c9b486ffd8
RaphaelDumas1 <96191736+RaphaelDumas1@users.noreply.github.com>
2022-10-11 17:30:01 +0200

    push debut jquery

--- test.js
+++ test.js
@@ -1,5 +1,38 @@
-import fetch from 'node-fetch';
+async function recuperNom() {
+    let database = []
+    await fetch('etudiants.json')
+        .then(res => res.json())
+        .then((data) => database = [...data.students])
+    console.log(database)
+    return database
+}
+function generateHtml(response) {
 
-fetch('etudiants.json')
-    .then(res => res.json())
-    .then((data) => console.log(data));
+}
+async function recupererDonnees() {
+    var eleves = [];
+    let datab = await recuperNom();
+    console.log(datab);
+    for (eleve of datab) {
+        console.warn(eleve);
+        await fetch(`https://api.github.com/users/${eleve}`, { headers: new Headers({ "Authorization": "Bearer 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" }) })
+            .then(res => res.json())
+            .then((data) => eleves.push(data))
+    }
+    eleves.forEach(element => {
+        let node = document.getElementById("test");
+        //node.innerHTML += " ID : " + response.login + ", Avatar : " + response.avatar_url + ", Nombre de repository : " + response.public_repos + ", Nombre de followers : " + response.followers
+        let img = $(".test").append("<img></img>");
+        img.src = element.avatar_url
+
+        let title = $(".test").append("<h2></h2>");
+        title.innerHTML = element.login;
+
+        let div = $(".test").append("<div></div>");
+        div.appendChild(title)
+        div.appendChild(img)
+
+        node.appendChild(div);
+    });
+
+}
